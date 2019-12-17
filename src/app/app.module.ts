95c9516be004b7dde3b13d718a3f86b23301acf0
invict1 <pabloinvict1@gmail.com>
2019-12-17 06:31:30 -0300

    componentes y carpeta con imagenes creados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { NgModule, OnInit } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import * as AOS from 'aos';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { WorkComponent } from './components/work/work.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CareersComponent } from './components/careers/careers.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    ServicesComponent,
+    WorkComponent,
+    AboutComponent,
+    TeamComponent,
+    TechnologiesComponent,
+    CareersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
