e4febce21d7640774e19dd0d4d0f90028e02d184
Guille Biatturi <gbiatturi@gmail.com>
2019-12-17 21:59:40 -0300

    - Config de custom Webpack  - Config de Tailwind CSS

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,26 +1,19 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.html', './src/**/*.component.ts'],
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  });
-  
-  module.exports = (config, options) => {
-    console.log(`Using '${config.mode}' mode`);
-    config.module.rules.push({
-      test: /tailwind\.scss$/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            plugins: [
-              require('tailwindcss')('./tailwind.config.js'),
-              require('autoprefixer'),
-              ...(config.mode === 'production' ? [purgecss] : [])
-            ]
+module.exports = {
+  module: {
+      rules: [
+          {
+              test: /\.scss$/,
+              loader: 'postcss-loader',
+              options: {
+                  ident: 'postcss',
+                  syntax: 'postcss-scss',
+                  plugins: () => [
+                      require('postcss-import'),
+                      require('tailwindcss'),
+                      require('autoprefixer'),
+                  ]
+              }
           }
-        }
       ]
-    });
-    return config;
-  };
+  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        'our-work-shadow': '-20px -20px 0px 0px rgba(0,0,0,0.75)'
+      }
+    },
   },
   variants: {},
   plugins: []
