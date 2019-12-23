442acb17cb719824d9e104404fa2f41ddfdf62f0
Guille Biatturi <gbiatturi@gmail.com>
2019-12-23 14:43:34 -0300

    - Mobile-first en componente Technologies  - Se generaliza código en angular  - FIX de height y width  - Se cambia estilo de iconos (bg white)

--- src/app/components/technologies/technologies.component.ts
+++ src/app/components/technologies/technologies.component.ts
@@ -7,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnologiesComponent implements OnInit {
 
+  technologies = [
+    {
+      image: '../../../assets/images/iconos/angular.png',
+      title: 'Angular'
+    },
+    {
+      image: '../../../assets/images/iconos/react.png',
+      title: 'React JS'
+    },
+    {
+      image: '../../../assets/images/iconos/vue.png',
+      title: 'Vue JS'
+    },
+    {
+      image: '../../../assets/images/iconos/node.png',
+      title: 'Node JS'
+    },
+    {
+      image: '../../../assets/images/iconos/ionic.png',
+      title: 'Ionic'
+    },
+    {
+      image: '../../../assets/images/iconos/blockchain.png',
+      title: 'Blockchain'
+    },
+    {
+      image: '../../../assets/images/iconos/ethereum.png',
+      title: 'Ethereum'
+    },
+    {
+      image: '../../../assets/images/iconos/python.png',
+      title: 'Python'
+    },
+    {
+      image: '../../../assets/images/iconos/csharp.png',
+      title: 'C#'
+    },
+    {
+      image: '../../../assets/images/iconos/java.png',
+      title: 'Java'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
