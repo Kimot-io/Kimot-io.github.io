edcb31050f27db952d155720d8a380eb11c4a282
Guille Biatturi <gbiatturi@gmail.com>
2019-12-23 11:13:18 -0300

    - Mobile-first componente Services.  - Se generaliza HTML  - Fix height flexbox

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  services = [
+    {
+      image: '../../../assets/images/others/web-service.png',
+      title: 'Web Development',
+      description: 'We have great experience in web development, we focus on performance and usability, both for browsers and mobile responsive using the latest technologies.'
+    },
+    {
+      image: '../../../assets/images/others/mobile-service.png',
+      title: 'Mobile Development',
+      description: 'We have great experience in web development, we focus on performance and usability, both for browsers and mobile responsive using the latest technologies.'
+    },
+    {
+      image: '../../../assets/images/others/ai-service.png',
+      title: 'Artificial Intelligence',
+      description: 'We have great experience in web development, we focus on performance and usability, both for browsers and mobile responsive using the latest technologies.'
+    },
+    {
+      image: '../../../assets/images/others/blockchain-service.png',
+      title: 'Blockchain',
+      description: 'We have great experience in web development, we focus on performance and usability, both for browsers and mobile responsive using the latest technologies.'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
